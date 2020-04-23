51ad4b2b5fdcc994a4d9b1d0ad4aff1f64619dd6
Julian Blasco <blascoblasco9@gmail.com>
2020-04-23 17:20:47 -0300

    Add username column and validations for user model. Modify devise views to accept the username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, uniqueness: true
   has_many :posts
 end
