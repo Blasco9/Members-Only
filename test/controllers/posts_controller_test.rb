3e8650d5b4ac30b11c73af957b96da800b81c429
Julian Blasco <blascoblasco9@gmail.com>
2020-04-23 16:57:01 -0300

    Remove unnecessary tests, actions and views for posts

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -22,27 +22,4 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to post_url(Post.last)
   end
-
-  test "should show post" do
-    get post_url(@post)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_post_url(@post)
-    assert_response :success
-  end
-
-  test "should update post" do
-    patch post_url(@post), params: { post: { body: @post.body, title: @post.title, user_id: @post.user_id } }
-    assert_redirected_to post_url(@post)
-  end
-
-  test "should destroy post" do
-    assert_difference('Post.count', -1) do
-      delete post_url(@post)
-    end
-
-    assert_redirected_to posts_url
-  end
 end
--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -22,26 +22,4 @@ class PostsTest < ApplicationSystemTestCase
     assert_text "Post was successfully created"
     click_on "Back"
   end
-
-  test "updating a Post" do
-    visit posts_url
-    click_on "Edit", match: :first
-
-    fill_in "Body", with: @post.body
-    fill_in "Title", with: @post.title
-    fill_in "User", with: @post.user_id
-    click_on "Update Post"
-
-    assert_text "Post was successfully updated"
-    click_on "Back"
-  end
-
-  test "destroying a Post" do
-    visit posts_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Post was successfully destroyed"
-  end
 end
