3f6114bf1f9dd6628abb2dd465c8a37fc444541f
Julian Blasco <blascoblasco9@gmail.com>
2020-04-23 17:09:25 -0300

    Add associations for user and post models. Modify posts #new and #create to reference the user id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:new, :create]
+
   # GET /posts
   # GET /posts.json
   def index
@@ -8,17 +9,17 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @post = current_user.posts.new
   end
 
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
